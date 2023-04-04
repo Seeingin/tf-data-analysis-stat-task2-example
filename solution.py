1763d4e5ee75e1067301ef485adb4bc025be02f9
Aleksandra Kuleva <129543447+Seeingin@users.noreply.github.com>
2023-04-04 11:46:49 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -7,11 +7,9 @@ from scipy.stats import norm
 chat_id = 752592494
 
 def solution(p: float, x: np.array) -> tuple:
-    from scipy.stats import t
-
     alpha = 1 - p
-    loc = np.mean(x)
-    scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
-    t_value = t.ppf(1 - alpha / 2, df=len(x) - 1)
-    interval = (loc - t_value * scale, loc + t_value * scale)
-    return interval
+    mean = x.mean()
+    standard_error = np.std(x, ddof=1) / np.sqrt(len(x))
+    z = norm.ppf(1 - alpha / 2)
+    confidence_interval = (mean - z * standard_error, mean + z * standard_error)
+    return confidence_interval
